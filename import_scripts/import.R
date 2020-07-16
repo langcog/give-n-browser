b9eb9c1c1f506e2c54f387d889298a707a2696f6
rosemschneider <rosemschneider@gmail.com>
2020-07-16 13:14:02 -0700

    updating for mcf

--- import_scripts/import.R
+++ import_scripts/import.R
@@ -1,53 +1,64 @@
 # schema:
-# 1. datasets: dataset_id, lab, cite, method
+# 1. datasets: dataset_id, lab, method, cite  
 # 2. subjects: subject_id, dataset_id, age, sex, language, kl (computed)
-# 3. trials: trial_id, subject_id, quantity, response, counted
-
+# 3. trials: trial_id, subject_id, quantity, response
+source('import_scripts/helper.R')
 library(magrittr)
 library(tidyverse)
 
-## read raw data 
-almoammer_raw <- read_csv(here::here("raw_data/Almoammer2013_english.csv"))
-piantadosi_raw <- read_csv(here::here("raw_data/Piantadosi2014.csv"))                      
+
+## TO-DO: set input and output paths more clearly
+
+## NB: in some cases, KL for participants will have to be added later
+
+## read in processed data
+kl_only_data <- read.csv(here::here("data/processed-data/kl_data.csv")) #data is cleaned and processed in another script
+trial_level_data <- read.csv(here::here('data/processed-data/trial_level_processed_data.csv'))
 
 ## ------------------- reformat and factor data to schema
 
-# delete processed data
-file.remove(here::here("processed_data/datasets.csv"))
-file.remove(here::here("processed_data/subjects.csv"))
-file.remove(here::here("processed_data/trials.csv"))
+# # delete processed data
+file.remove(here::here("data/processed-data/datasets.csv"))
+file.remove(here::here("data/processed-data/subjects.csv"))
+file.remove(here::here("data/processed-data/trials.csv"))
 
-
-###### almoammer
-almoammer <- almoammer_raw %>%
-  gather(quantity, response, `1`:`3_2`) %>%
-  mutate(quantity = as.numeric(unlist(map(almoammer$quantity,
-                                          function(x) {str_split(x, pattern = "_")[[1]][1]}))))
-
-almoammer %>% 
-  select(Experiment, Participant, Language, PrimaryLang, Age_months, SEX) %>%
-  distinct() %>%
-  rename(dataset_id = Experiment, 
-         subject_id = Participant, 
-         language = Language, 
-         age = Age_months, 
-         sex = SEX) %>%
-  write_to_subjects()
-
-almoammer %>%
-  select(Participant, quantity, response) %>%
-  rename(subject_id = Participant) %>%
-  write_to_trials()
-
-almoammer %>%
-  select(Experiment) %>%
-  rename(dataset_id = Experiment) %>%
-  mutate(lab = "Barner") %>%
+# KL-only data ----
+##datasets
+kl_only_data %>%
+  dplyr::select(Experiment, lab, method, cite) %>%
+  droplevels()%>%
+  dplyr::rename(dataset_id = Experiment) %>%
   write_to_datasets()
 
-###### piantadosi
+##subjects
+##To-DO: Get Sex information
+kl_only_data %>% 
+  select(Experiment, Subject, Language, Age_months, KL) %>%
+  distinct() %>%
+  rename(dataset_id = Experiment, 
+         subject_id = Subject, 
+         language = Language, 
+         age = Age_months) %>%
+  write_to_subjects()
 
+## Trial-level data ----
+## function for trial_id
+create_zero_index<- function(data, id_column_name="Subject") {
+  data <- data %>%
+    mutate(query_lag = lag(Query), 
+           temp = ifelse(Query != query_lag, 1, 0), 
+           temp_id = cumsum(c(0, temp[!is.na(temp)])), 
+           trial_id = temp_id)
+}
 
+trial_level_data <- create_zero_index(trial_level_data)
 
+trial_level_data %>%
+  dplyr::select(Experiment, trial_id, Subject, Query, Response)%>%
+  dplyr::rename(dataset_id = Experiment, 
+                subject_id = Subject)%>%
+  write_to_trials()
 
 ## ------------------- run KL models on everyone and add data to subjects
+
+#
