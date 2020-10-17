263eab8784932342cefbb9e0eec9484c5782f488
rosemschneider <rosemschneider@gmail.com>
2020-10-17 15:19:33 -0700

    good progress on Wynn KL - need to fix discrepancies

--- import_scripts/import.R
+++ import_scripts/import.R
@@ -56,17 +56,6 @@ trial_level_data %>%
 # --- run KL models on everyone
 ## ... Wynn KL assignment ----
 ## This will only be run with trial-level data 
-##get 0s and 1s for responses
-tmp <- trial_level_data%>%
-  filter(Experiment == "Almoammer2013")%>%
-  mutate(correct = ifelse(Query == Response, 1, 0))
-
-#this will work for non-titrated
-tmp1 <- tmp %>%
-  group_by(Subject, Query)%>%
-  summarise(num_trials = n(), 
-            num_correct = sum(correct))
-
 create_wynn_df <- function(df) {
   full_wynn_df <- data.frame() #placeholder df
   
@@ -78,7 +67,7 @@ create_wynn_df <- function(df) {
       filter(Experiment == d)
     unique_queries <- as.vector(unique(d.subset$Query)) #get unique queries for false gives
     
-    d.subset %<>% #this gives us false gives within a particular datset
+    d.subset <- d.subset %>% #this gives us false gives within a particular datset
       mutate(false_give = ifelse((correct == 0 & Response %in% unique_queries), 
                                  1, 0), 
              resp_false_give = ifelse(false_give == 1, Response, NA))
@@ -99,10 +88,85 @@ create_wynn_df <- function(df) {
       mutate(dataset = d)
     full_wynn_df <- bind_rows(full_wynn_df, tmp_wynn_df)
   }
+  full_wynn_df <- full_wynn_df %>%
+    mutate(num_failures = num_trials - num_correct, 
+           num_false_give = ifelse(is.na(num_false_give), 0, as.numeric(num_false_give)))
   return(full_wynn_df)
 }
 
-x <- create_wynn_df(trial_level_data) #hooray this works!
+final_wynn_df <- create_wynn_df(trial_level_data) #hooray this works!
 
+##okay now we need to assign KLs
+tmp_assignment <- final_wynn_df %>%
+  mutate(possibly_known = ifelse(((num_correct/num_trials >= 2/3) & num_false_give == 0), 1, #if they are successful 2/3 of the time with no false gives
+                                 ifelse(((num_correct/num_trials >= 2/3) & (num_correct/(num_correct+num_false_give)) >= 2/3), 1, 0)), #if they are successful 2/3 time and of times they gave n it was in response to n at least 2/3 of the time
+         definitely_unknown = ifelse(((num_correct/num_trials) < 2/3), 1, 
+                                     ifelse(((num_correct/(num_correct+num_false_give)) < 2/3), 1, 0)), 
+         tracker = ifelse((possibly_known == 1 & definitely_unknown == 0), 1, 
+                          ifelse((possibly_known == 0 & definitely_unknown == 1), 0, 
+                                 ifelse((possibly_known == 1 & definitely_unknown == 1), "HELP", 
+                                        ifelse((possibly_known == 0 & definitely_unknown == 0), "CHECK", "Other"))))) 
+
+##TODO - write function that checks for issues
+
+track_successes <- function(df) {
+  tmp.df <- df %>%
+    mutate(success_tracker = ifelse((tracker == 0 & Query == 1), -100, #if this is the first # and tracker is 0, NON
+                                    ifelse(tracker == 1, Query, #otherwise, keep track of last successful number
+                                           ifelse((tracker == 0 & lag(tracker) == 1), -50, 
+                                                  ifelse((tracker == 0 & lag(tracker) == 0), -50, "help")))))
+  
+  ##TODO - add function that will throw error if there is a help in this df
+  
+  unique_subs <- as.vector(unique(tmp.df$Subject))
+  tracker.df <- data.frame()
+  for (s in unique_subs) {
+    s.df <- tmp.df %>%
+      filter(Subject == s)
+    
+    if (min(s.df$success_tracker) == -100) {
+      tmp.s.df <- data.frame(Subject = s, 
+                     Assignment = '0')
+    } else if (min(s.df$success_tracker) > 0) { #if they haven't gotten anything wrong, and there are no NAs
+      highest_num <- max(s.df$success_tracker)
+      tmp.s.df <- data.frame(Subject = s, 
+                             Assignment = as.character(highest_num))
+    } else if (min(s.df$success_tracker) == -50) { #if they have gotten something incorrect 
+      #then we should get the *minimum* number for which they failed
+      min.fail <- s.df %>%
+        filter(success_tracker == -50)
+      
+      min.fail.num <- min(min.fail$Query) #get the minimum number for which they failed
+      
+      #filter out anything in main df above this
+      s.df <- s.df %>%
+        filter(Query < min.fail.num)
+      
+      #now get the maximum number in their success tracker after this
+      max.succeed.num <- max(s.df$success_tracker)
+      
+      tmp.s.df <- data.frame(Subject = s, 
+                             Assignment = as.character(max.succeed.num))
+    } else {
+      tmp.s.df <- data.frame(Subject = s, 
+                             Assignment = '-500')
+    }
+    tracker.df <- bind_rows(tracker.df, tmp.s.df)
+  }
+  return(tracker.df)
+  }
+
+checking_assignments <- track_successes(tmp_assignment) %>%
+  mutate(Assignment = ifelse(as.numeric(as.character(Assignment)) >= 6, "CP", as.character(Assignment)))
+
+##TODO - special cases for CP??
+
+#bind with original for checking
+tmp_check <- left_join(all_data, checking_assignments, by = "Subject")%>%
+  mutate(KL_check = ifelse(KL != Assignment, "FLAG", "Fine"))
+
+flags <- tmp_check %>%
+  filter(KL_check == "FLAG")%>%
+  distinct(Subject, Experiment, KL,Assignment)
 
 
