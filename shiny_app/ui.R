05c5e6efdd5429224ba30a3e350ff6865200a7ea
rosemschneider <rosemschneider@gmail.com>
2021-01-29 12:53:16 -0800

    update for about give-n

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -1,46 +1,68 @@
-library(shinyBS)
 library(shinythemes)
 library(shinyWidgets)
+library(shiny)
+library(leaflet)
+library(plotly)
+library(shinyjs)
+library(shinyBS)
 
-ui <- fluidPage(
-  theme = shinytheme("spacelab"),
-  
-  # bsCollapse(id = "doc", open = "title",
-  #            bsCollapsePanel(title = h3("Numberbank"),
-  #                            # includeMarkdown("../docs/peekbank.md"),
-  #                            value = "title",
-  #                            style = "default")
-  # ),
-  
-  navbarPage("Numberbank", theme = shinytheme("paper"),
-             tabPanel("Knower levels", fluid = TRUE,
-                      # Sidebar layout with a input and output definitions
-                      sidebarLayout(
-                        sidebarPanel(
-                              uiOutput("age_range_selector"),
-                              uiOutput("language_selector"),
-                              uiOutput("method_selector") , 
-                              uiOutput("kl_range_selector"), 
-                              
-                              downloadButton("downloadData", "Download")
-                            ),
-    
-    mainPanel(
-      tags$style(type = "text/css",
-                 ".shiny-output-error { visibility: hidden; }",
-                 ".shiny-output-error:before { visibility: hidden; }"),
-      tabsetPanel(selected = "KL by age, language", 
-                  tabPanel("KL by age, language",
-                           plotOutput("age_boxplot")), 
-                  tabPanel("Datasets",
-                           htmlOutput("citations"))
-                  ), 
-      tabsetPanel(selected = "Cumulative probability of KL", 
-                  tabPanel("Cumulative probability of KL",
-                           plotOutput("cumulative_prob"))
-                  ) 
-              )
-        )
+### . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
+### Colors                                                                  ####
+
+#C10250 purple
+#03BCC0 green
+#D2D945 yellow/green
+#FCB040 orange
+#FF5850 red
+#436983 hipster blue
+
+shinyUI(navbarPage(title = "Numberbank",
+                   theme = "spacelab",
+                   fluid = TRUE, 
+                   collapsible = TRUE,
+                   
+   # ----------------------------------
+   # tab panel 1 - Home
+   tabPanel("Home",
+            includeHTML("home.html"),
+            tags$head(
+              tags$link(rel = "stylesheet", 
+                        type = "text/css", 
+                        href = "plugins/font-awesome-4.7.0/css/font-awesome.min.css"),
+              tags$link(rel = "icon", 
+                        type = "image/png", 
+                        href = "img/fishwithears.png")
+            )
+   ),
+                   
+   tabPanel("Knower levels", fluid = TRUE,
+            # Sidebar layout with a input and output definitions
+            sidebarLayout(
+              sidebarPanel(
+                    uiOutput("age_range_selector"),
+                    uiOutput("language_selector"),
+                    uiOutput("method_selector") , 
+                    uiOutput("kl_range_selector"), 
+                    
+                    downloadButton("downloadData", "Download")
+                  ),
+
+      mainPanel(
+        tags$style(type = "text/css",
+                   ".shiny-output-error { visibility: hidden; }",
+                   ".shiny-output-error:before { visibility: hidden; }"),
+        tabsetPanel(selected = "KL by age, language", 
+                    tabPanel("KL by age, language",
+                             plotOutput("age_boxplot")), 
+                    tabPanel("Datasets",
+                             htmlOutput("citations"))
+                    ), 
+        tabsetPanel(selected = "Cumulative probability of KL", 
+                    tabPanel("Cumulative probability of KL",
+                             plotOutput("cumulative_prob"))
+                    ) 
+                )
+          )
    ), 
     
     tabPanel("Item visualizations", fluid = TRUE,
@@ -70,24 +92,31 @@ ui <- fluidPage(
                 tabsetPanel(selected = "Responses by language", 
                             tabPanel("Responses by language",
                                      plotOutput("lang_histogram"))
-                            ), 
-                tabsetPanel(selected = "Responses by KL", 
-                            tabPanel("Responses by KL",
-                                     plotOutput("kl_histogram"))
-                )
+                            )
               )
           )
         ) 
     ), 
    tabPanel("About Give-N", fluid = TRUE,
-            tabPanel("About Give-N", fluid = TRUE,
-              mainPanel("Description of Give-N")
+            includeHTML("about_given.html"),
+            tags$head(
+              tags$link(rel = "stylesheet", 
+                        type = "text/css", 
+                        href = "plugins/font-awesome-4.7.0/css/font-awesome.min.css"),
+              tags$link(rel = "icon", 
+                        type = "image/png", 
+                        href = "img/fishwithears.png")
             )
         ), 
    tabPanel("Contributors", fluid = TRUE,
             tabPanel("Contributors", fluid = TRUE,
                      mainPanel("Contributors go here")
             )
+      ), 
+  tabPanel("About Numberbank", fluid = TRUE,
+           tabPanel("About Numberbank", fluid = TRUE,
+                    includeHTML("about.html")
+           )
       )
   )
 )
